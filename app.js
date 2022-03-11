b6b916692265f0363119c5d88892b924836279bf
Imari Childress <imaricodes@gmail.com>
2022-03-11 08:18:25 -0500

    Remove hover css from 'pixel' elements and add mouseover event that changes background color of 'pixel'.

--- app.js
+++ app.js
@@ -3,21 +3,49 @@ const rowDiv = document.createElement('div');
 let pixelFrag = document.createDocumentFragment();
 
 console.log(container);
+
+
+
 function createRowDiv () {
     rowDiv.classList.add('rowDiv');
     container.appendChild(rowDiv);
-    
+    createPixels();
+      
 }
 
 function createPixels () {
-    
+    let createdPixel;
     for (let i = 0; i < 10; i++) {
-       
+        
         const pixel = document.createElement('div');
         pixel.classList.add('pixel-specs');
         pixel.textContent = "P";
-        pixelFrag.appendChild(pixel);
+        pixelFrag.appendChild(pixel);  
     }
     rowDiv.appendChild(pixelFrag);
+    console.log(rowDiv);
+    createdPixel = document.querySelectorAll('.pixel-specs');
+    console.log(createdPixel.length);
+    console.log("created pixel = " + createdPixel);
+
+   createdPixel.forEach(element => {
+       element.addEventListener('mouseover', function (e) {
+           console.log('uck');
+           element.classList.add('turn-orange');
+           e.stopImmediatePropagation
+       });
+   });
+
+
+    
+   
+   
 
 }
+
+
+
+
+
+
+
