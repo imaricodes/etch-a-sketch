d39401049d3bbb80ac69766e183a21d374b64a33
Imari Childress <imaricodes@gmail.com>
2022-03-13 14:38:49 -0400

    Commit before branch.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 const containerHeight = 400;
 const containerWidth = 400;
-let gridSize = 100;
+let gridSize = 10;
 
 
 
@@ -45,6 +45,37 @@ function createPixels (rowDiv) {
     } 
 
 }
+
+// let mouseDown = false
+// document.body.onmousedown = () => (mouseDown = true)
+// document.body.onmouseup = () => (mouseDown = false)
+
+// for (let i = 0; i < size * size; i++) {
+//     const gridElement = document.createElement('div')
+//     gridElement.classList.add("grid-element")
+//     gridElement.addEventListener('mouseover', changeColor)
+//     gridElement.addEventListener('mousedown', changeColor)
+//     grid.appendChild(gridElement)
+//   }
+
+
+// function changeColor(e) {
+//     if (e.type === 'mouseover' && !mouseDown) return
+//     if (currentMode === 'rainbow') {
+//       const randomR = Math.floor(Math.random() * 256)
+//       const randomG = Math.floor(Math.random() * 256)
+//       const randomB = Math.floor(Math.random() * 256)
+//       e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+//     } else if (currentMode === 'color') {
+//       e.target.style.backgroundColor = currentColor
+//     } else if (currentMode === 'eraser') {
+//       e.target.style.backgroundColor = '#fefefe'
+//     }
+//   }
+
+
+
+
 
 
 
