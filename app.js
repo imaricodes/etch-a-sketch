35f6bd676b7fe17f64092392bcc0dc6eaa5afabb
Imari Childress <imaricodes@gmail.com>
2022-03-16 07:14:51 -0400

    Commit before merging to main.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const container = document.querySelector('.container');
 const containerHeight = 400;
 const containerWidth = 400;
-let gridSize = 10;
+let colorMode = "none";
+
+
 
 
 function createGrid () {
+    clearGrid();
     let rowFrag = document.createDocumentFragment();
-
+    let gridSize = document.getElementById('set-size').value;
+    console.log(gridSize);
     for (let i = 0; i < gridSize; i++) {
         const rowDiv = document.createElement('div');
         
@@ -14,44 +18,119 @@ function createGrid () {
         
         rowFrag.appendChild(rowDiv);
         rowDiv.style.height = containerHeight/gridSize + "px";
-        createPixels(rowDiv); 
+        createPixels(rowDiv, gridSize); 
         container.appendChild(rowFrag);
 
         //add listener to elements
+        let createdPixels = document.querySelectorAll('.pixel');
         let mouseDown = false
         document.body.onmousedown = () => (mouseDown = true);
         document.body.onmouseup = () => (mouseDown = false);
-        let createdPixels = document.querySelectorAll('.pixel');
+
+        const eraserBtn = document.getElementById('erase')
+        
+        
+
+        //check color mode first? onclick set value in 
+        
+                
         createdPixels.forEach(element => {
             function draw (e) {
                 if (e.type === 'mouseover' && !mouseDown) return
-                element.classList.add('turn-orange');
-            }
+                //eraser class
+                
+                
+                //if rainbow true, run rainbow function
+                
+                //else set color to color pick
+                // if (colorMode === "draw mode") {
+                //     let colorPick = document.getElementById('color-pick').value;
+                    
+                //     e.target.style.backgroundColor = colorPick;
+                // }
+                if (colorMode === "draw mode") {
+                    let colorPick = document.getElementById('color-pick').value;
+                    
+                    e.target.style.backgroundColor = colorPick;
+                    
+                }
 
+                else if (colorMode === "erase mode") {
+                    e.target.style.backgroundColor = "#9F2B68";
+                }
+
+                // else {
+                //     let colorPick = document.getElementById('color-pick').value;
+                    
+                //     e.target.style.backgroundColor = colorPick;
+                // }
+                
+                
+                
+               
+            }
+            
             element.addEventListener('mouseover', draw);
         });
     } 
 }
 
-function createPixels (rowDiv) {
+function createPixels (rowDiv, gridSize) {
     let pixelFrag = document.createDocumentFragment();
     
     for (let i = 0; i < gridSize; i++) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
         pixelFrag.appendChild(pixel);
-        console.log("pixel frag: " + pixelFrag);
         rowDiv.appendChild(pixelFrag);
        
     } 
 }
 
 function clearGrid () {
-    let createdPixels = document.querySelectorAll('.pixel');
-    createdPixels.forEach(element => {
-        element.classList.remove('turn-orange');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function eraseGrid () {
+    
+    let pixelsToErase = document.querySelectorAll('.pixel');
+    pixelsToErase.forEach(element => {
+        console.log(element);
+        element.style.backgroundColor = "#FFFFFF";
     })
 }
+
+let input = document.getElementById("color-pick");
+function setColorMode (e) {
+
+    input.addEventListener('input', function () {
+        colorMode = "draw mode";
+        console.log("draw mode");
+        return "draw mode"
+    });
+
+    
+    
+    if (e.target.id === "erase") {
+        colorMode = "erase mode"
+        console.log('erase button clicked');
+        console.log(e.target.id);
+        return "erase"  
+    }
+    else if (e.target.id === "rainbow") {
+        console.log('rainbow button clicked');
+        console.log(e.target.id);
+        return "rainbow"  
+    }
+
+    
+}
+
+window.onload = () => {
+    createGrid();
+  }
 
 
 
