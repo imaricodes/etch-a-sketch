39d3146b8e761495ce7be7c2bbd95b7cf16be420
Imari Childress <imaricodes@gmail.com>
2022-03-16 08:46:05 -0400

    Add toggle.

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ let colorMode = "none";
 
 
 
+let createdPixels = document.querySelectorAll('.pixel');
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function draw (e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    if 
+    let colorPick = document.getElementById('color-pick').value;
+    e.target.style.backgroundColor = colorPick;
+    
+    
+}
+
+
 function createGrid () {
     clearGrid();
     let rowFrag = document.createDocumentFragment();
@@ -13,77 +29,24 @@ function createGrid () {
     console.log(gridSize);
     for (let i = 0; i < gridSize; i++) {
         const rowDiv = document.createElement('div');
-        
         rowDiv.classList.add('rowDiv');
-        
         rowFrag.appendChild(rowDiv);
         rowDiv.style.height = containerHeight/gridSize + "px";
         createPixels(rowDiv, gridSize); 
-        container.appendChild(rowFrag);
-
-        //add listener to elements
-        let createdPixels = document.querySelectorAll('.pixel');
-        let mouseDown = false
-        document.body.onmousedown = () => (mouseDown = true);
-        document.body.onmouseup = () => (mouseDown = false);
-
-        const eraserBtn = document.getElementById('erase')
-        
-        
-
-        //check color mode first? onclick set value in 
-        
-                
-        createdPixels.forEach(element => {
-            function draw (e) {
-                if (e.type === 'mouseover' && !mouseDown) return
-                //eraser class
-                
-                
-                //if rainbow true, run rainbow function
-                
-                //else set color to color pick
-                // if (colorMode === "draw mode") {
-                //     let colorPick = document.getElementById('color-pick').value;
-                    
-                //     e.target.style.backgroundColor = colorPick;
-                // }
-                if (colorMode === "draw mode") {
-                    let colorPick = document.getElementById('color-pick').value;
-                    
-                    e.target.style.backgroundColor = colorPick;
-                    
-                }
-
-                else if (colorMode === "erase mode") {
-                    e.target.style.backgroundColor = "#9F2B68";
-                }
-
-                // else {
-                //     let colorPick = document.getElementById('color-pick').value;
-                    
-                //     e.target.style.backgroundColor = colorPick;
-                // }
-                
-                
-                
-               
-            }
-            
-            element.addEventListener('mouseover', draw);
-        });
+        container.appendChild(rowFrag);         
     } 
 }
+
 
 function createPixels (rowDiv, gridSize) {
     let pixelFrag = document.createDocumentFragment();
     
     for (let i = 0; i < gridSize; i++) {
         let pixel = document.createElement('div');
+        pixel.addEventListener('mouseover', draw);
         pixel.classList.add('pixel');
         pixelFrag.appendChild(pixel);
-        rowDiv.appendChild(pixelFrag);
-       
+        rowDiv.appendChild(pixelFrag);     
     } 
 }
 
@@ -127,6 +90,15 @@ function setColorMode (e) {
 
     
 }
+
+let drawToggle = document.getElementById('draw-toggle');
+drawToggle.addEventListener('click',() => {
+
+    if (drawToggle.checked == false) {
+        console.log("it's false");
+    }
+    console.log("boof");
+})
 
 window.onload = () => {
     createGrid();
