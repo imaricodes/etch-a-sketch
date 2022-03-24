f52acc4192b498e1233f398035f27df418457c9c
Imari Childress <imaricodes@gmail.com>
2022-03-24 09:35:21 -0400

    Add range input slider to change grid resolution.

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const container = document.querySelector('.container');
 const containerHeight = 800;
 const containerWidth = 800;
-let colorMode = "default";
 
+//DRAWING FUNCTIONS
+let colorMode = "default";
 let drawmode = "default";
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+let mouseDown = false
+
+
+function draw (e) {
+    
+    let colorPick = document.getElementById('color-pick').value;
+   
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    if (drawmode === "default" && colorMode === "default") {
+        
+        e.target.style.backgroundColor = colorPick;
+    }
+    else if (drawmode === "default" && colorMode === "rainbow" ) {
+       
+        e.target.style.backgroundColor = "#000000";
+        
+    }
+    else if (drawmode === "erasemode") {
+        e.target.style.backgroundColor = "#FFFFFF";
+    }   
+}
+
 
 let drawToggle = document.getElementById('draw-toggle');
-
-
 drawToggle.addEventListener('click',() => {
 
     if (drawToggle.checked === false) {
@@ -20,46 +44,32 @@ drawToggle.addEventListener('click',() => {
     } 
 });
 
+function setColorMode (e) {
+    resetDrawMode();
+    if (e.target.id === "rainbow") {
+        colorMode = "rainbow";
+        
+    } 
+}
 
 function resetDrawMode () {
     drawToggle.checked = false;
-    drawmode = "default";
-    
+    drawmode = "default";  
 }
 
+function resetColorMode(){
+    colorMode = "default";
+}
+
+
+//CREATE GRID
 let createdPixels = document.querySelectorAll('.pixel');
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
-let mouseDown = false
-
-function draw (e) {
-    
-    let colorPick = document.getElementById('color-pick').value;
-    console.log("color mode: " + colorMode + " drawmode:"  + drawmode)
-    if (e.type === 'mouseover' && !mouseDown) return
-
-    if (drawmode === "default" && colorMode === "default") {
-        
-        e.target.style.backgroundColor = colorPick;
-    }
-    else if (drawmode === "default" && colorMode === "rainbow" ) {
-        console.log("magic rainbowmode!");
-        e.target.style.backgroundColor = "#000000";
-        
-    }
-    else if (drawmode === "erasemode") {
-        e.target.style.backgroundColor = "#FFFFFF";
-    }
-    
-}
-
-
 
 function createGrid () {
     resetDrawMode();
     clearGrid();
     let rowFrag = document.createDocumentFragment();
-    let gridSize = document.getElementById('set-size').value;
+    let gridSize = document.getElementById('resolution-slider').value;
     
     for (let i = 0; i < gridSize; i++) {
         const rowDiv = document.createElement('div');
@@ -69,7 +79,6 @@ function createGrid () {
         createPixels(rowDiv, gridSize); 
         container.appendChild(rowFrag);         
     }
-    console.log("after grid change: " + colorMode); 
 }
 
 
@@ -85,35 +94,34 @@ function createPixels (rowDiv, gridSize) {
     } 
 }
 
-function clearGrid () {
+//MODIFY GRID
+let resolutionValue = document.getElementById('resolution-slider').value;
+let resolutionDisplay = document.querySelector('.resolution-display');
+let resolutionSlider = document.getElementById('resolution-slider');
+resolutionSlider.addEventListener('input', changeResolution);
+
+function changeResolution (){
+    let resolutionValue = document.getElementById('resolution-slider').value;
+    resolutionDisplay.innerText = resolutionValue + " x " + resolutionValue;
+    createGrid(); 
+}
+
+function eraseGrid () {
     
+    resetDrawMode();
+    let pixelsToErase = document.querySelectorAll('.pixel');
+    pixelsToErase.forEach(element => { 
+        element.style.backgroundColor = "#FFFFFF";
+    })
+}
+
+function clearGrid () {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
 
-function eraseGrid () {
-    console.log(drawToggle.checked);
-    resetDrawMode();
-    let pixelsToErase = document.querySelectorAll('.pixel');
-    pixelsToErase.forEach(element => {
-       
-        element.style.backgroundColor = "#FFFFFF";
-    })
-}
 
-function resetColorMode(){
-    colorMode = "default";
-}
-
-//let input = document.getElementById("rainbow");
-function setColorMode (e) {
-    resetDrawMode();
-    if (e.target.id === "rainbow") {
-        colorMode = "rainbow";
-        
-    } 
-}
 
 window.onload = () => {
     createGrid();
